fc8ccbb2ee87ba92f344a488b24ffc0849b5f284
Manuel Alabor <msites@msites.net>
2012-09-10 14:58:40 +0300

    Update src/net/msites/kaffeekasse/pdf/ConsumptionSheetPDFFactory.java

--- src/net/msites/kaffeekasse/pdf/ConsumptionSheetPDFFactory.java
+++ src/net/msites/kaffeekasse/pdf/ConsumptionSheetPDFFactory.java
@@ -63,12 +63,12 @@ public class ConsumptionSheetPDFFactory extends AbstractPdfFactory {
         cell.setBorderWidthTop(0f);
         table.addCell(cell);
         
-        // Produkte hinzufügen:
+        // Produkte hinzufï¿½gen:
         for(Product product: products) table.addCell(createProductHeaderCell(product, language));
         table.completeRow();
         table.setHeaderRows(1);
         
-        // Konsumenten hinzufügen:
+        // Konsumenten hinzufï¿½gen:
         for(int i = 0, l = consumers.size(); i < l; i++) {
         	Consumer consumer = consumers.get(i);
         	cell = new PdfPCell(new Phrase(consumer.getFullname(), FONT_VERDANA_10_BOLD));
@@ -127,64 +127,5 @@ public class ConsumptionSheetPDFFactory extends AbstractPdfFactory {
 		
 		return para;
 	}
-	
-//	/**
-//	 * @param args
-//	 */
-//	public static void main(String[] args) throws Exception {
-//		ArrayList<Consumer> consumers = new ArrayList<Consumer>();
-//		ArrayList<Product> products = new ArrayList<Product>();
-//		
-//		consumers.add(new Consumer("Carmen", "Ackermann", "carmen.ackermann-goeldi@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Manuel", "Alabor", "manuel.alabor@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Renato", "Altenburger", "renato.altenburger@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Ravi", "Anthamola", "ravikumar.anthamola@credit-suisse.com", Language.ENGLISH, true));
-//		consumers.add(new Consumer("Gerry", "Arnold", "gerhard.arnold@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Roger", "Begert", "roger.w.begert@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Patrick", "Belk", "patrick.belk@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Josip", "Budzaki", "josip.budzaki@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Mario", "Clavadetscher", "mario.clavadetscher@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Mario", "Eugster", "mario.eugster@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Hermann", "Gloor", "hermann.gloor@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Christoph", "Glutz", "christoph.glutz@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Satish", "Kessettiv", "satish.k.kessettiv@credit-suisse.com", Language.ENGLISH, true));
-//		consumers.add(new Consumer("Corsin", "Marques", "corsin.marques@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Jeremias", "Messmer", "jeremias.messmer@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Sandro", "Muggli", "sandro.muggli@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Pius", "Pally", "pius.pally@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Daniel", "Rensch", "daniel.rensch@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Marcel", "Schnoz", "marcel.schnoz@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Lars", "Schröder", "lars.schroeder@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Stefan", "Ort", "stefan.ort@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Roger", "Steinmann", "roger.steinmann@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Lorenz", "Tanner", "lorenz.tanner@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Cindy", "Weber", "cindy.weber@credit-suisse.com", Language.GERMAN, true));
-//		consumers.add(new Consumer("Madhu", "Yalamuri", "madhusudhan.yalamuri@credit-suisse.com", Language.ENGLISH, true));
-//
-//		products.add(new Product("Tee / Milch", 0.5d));
-//		products.add(new Product("Kaffee", 0.8d));
-//		products.add(new Product("Glacé", 1d));
-//		products.add(new Product("Schokolade (klein)", 0.4d));
-//		products.add(new Product("Schokolade (gross)", 0.9d));
-//		products.add(new Product("Getränk", 1.2d));
-//		products.add(new Product("RedBull", 1.7d));
-////		products.add(new Product("Parkplatz", 5d));
-//		
-//		
-//		float[] widths = {
-//				3f,
-//				2f,
-//				3.5f,
-//				1f,
-//				1.6f,
-//				1.6f,
-//				3.5f,
-//				2f
-//		};
-//		
-//		
-//		
-//		new ConsumptionSheetPDFFactory().createSheet(consumers, products, widths);
-//	}
 
 }
